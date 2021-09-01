c1ae52612d435b9596122b944bef941928e0ad69
AnaMarquez <anamarquez.edu@hotmail.com>
2021-08-31 21:48:50 -0700

    counting Operations

--- BigONotation/04 Counting Operations/countingOperations.py
+++ BigONotation/04 Counting Operations/countingOperations.py
@@ -1,16 +1,16 @@
-student_list = ['Ana','Ale','Adriana','Santiago','Lidia']
+student_list = ['Ana','Ale','Adriana','Santiago','Lidia'] #O(1)
 
 def randomFunction(student_list):
-    firs = student_list[0]
-    total = 0
-    new_list = []
+    first = student_list[0] #O(1)
+    total = 0 #O(1)
+    new_list = [] #O(1)
 
-    for student in student_list:
-        total +=1
-        new_list.append(student)
+    for student in student_list: 
+        total +=1 #O(n)
+        new_list.append(student) #O(n)
 
-    print(new_list)
-    return total
+    print(new_list)#O(1)
+    return total#O(1)
 
     
-print(randomFunction(student_list))
+print(randomFunction(student_list))#O(6 + 2n) = O(n)
